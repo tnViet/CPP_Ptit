4196d2f015bb0006d43cf88e9d4fbd485ebbe49e
Trần Ngọc Việt <ngocviet2406@gmail.com>
2025-02-27 23:06:02 +0700

    Add files via upload

---
+++ cpp0116 uoc so nguyen to nho nhat.cpp
@@ -0,0 +1,28 @@
+// https://code.ptit.edu.vn/student/question/CPP0116
+
+#include <bits/stdc++.h>
+using namespace std;
+int prime(int n){
+	if(n==1) return 1;
+	if (n%2==0) return 2;
+	for(int i=3; i<=sqrt(n); i++)
+		if(n%i==0) return i;
+	return n;
+}
+void solve(int n)
+{
+	for(int i=1; i<=n; i++)
+		cout << prime(i) <<" ";
+	cout << endl;
+}
+int main()
+{
+	int t; cin >> t;
+	while(t--)
+	{
+		int n; cin >>n;
+		solve(n);
+		
+	}
+	return 0;
+}
