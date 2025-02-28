3b280aeaef419be6cf7b361dcb52da8ba7bfcffe
Trần Ngọc Việt <ngocviet2406@gmail.com>
2025-02-28 23:01:18 +0700

    Add files via upload

---
+++ cpp0120 chia het cho a va b.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve()
+{
+	int m, n, a, b;
+	cin >> m >> n >> a >> b;
+	int cnt=0;
+	for(int i=m; i<=n; i++)
+		if(i%a==0 || i%b==0 ) cnt++;
+	cout << cnt <<endl;
+}
+int main()
+{
+	int t; 	cin  >> t;
+	while(t--)
+	{
+		solve();
+	}
+	return 0;
+}
