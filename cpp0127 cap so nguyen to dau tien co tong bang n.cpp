e8e83fb4f26ae02625c7c6bbd4936cd114d57451
Trần Ngọc Việt <ngocviet2406@gmail.com>
2025-03-06 00:14:05 +0700

    Add files via upload

---
+++ cpp0127 cap so nguyen to dau tien co tong bang n.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int t,n,kt;
+int nt(int a){
+	if (a==2) return 1;
+	for (int i=2;i<=sqrt(a)+1;i++){
+		if (a%i==0) 
+			return 0;		
+	}
+	return 1;
+}
+int main(){
+	cin>>t;
+	while(t--){
+		cin>>n;kt=0;
+		for (int i=2;i<=n/2;i++){
+			if (nt(i)==1){
+				if (nt(n-i)==1){
+					cout<<i<<" "<<n-i<<endl;
+					kt=1;
+				}
+				if (kt==1) break;
+			}
+		}	
+		if (kt==0) cout<<"-1"<<endl;
+	}
+	return 0;
+}
